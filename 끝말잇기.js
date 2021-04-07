7af7dfcfc9e0bae918c254471fbf16c91fceda58
qoridhc <67674337+qoridhc@users.noreply.github.com>
2021-04-07 14:02:33 +0900

    Add files via upload

---
+++ 끝말잇기.js
@@ -0,0 +1,23 @@
+var 바디 = document.body;
+var 단어 = document.createElement('div');
+단어.textContent = '제로초';
+document.body.append(단어);
+
+//var 입력창 = document.createElement('input');
+document.body.append(document.createElement('input'))
+
+var 버튼 = document.createElement('button');
+document.body.append(버튼);
+
+/*
+var word = '제로초'
+
+while(true){
+    var answer = prompt(word);
+if(word[word.length - 1] === answer[0]){
+    alert("딩동댕");
+}else{
+alert("땡");
+}
+}
+*/
