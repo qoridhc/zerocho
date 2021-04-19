17f57b45b5ef1f80901e04c105387c28236eeebf
qoridhc <67674337+qoridhc@users.noreply.github.com>
2021-04-19 20:44:36 +0900

    Add files via upload

--- 가위바위보.js
+++ 가위바위보.js
@@ -1,40 +1,34 @@
-var left = 0;
+var 컴퓨터 = 0;
+var 딕셔너리 ={
+    바위 : '0',
+    가위 : '133px',
+    보 : '-288px'
+};
 
-//  setInterval(function 함수(){
-//      if(left === 0){
-//          left = '-133px'; 
-//      }else if(left === '-133px'){
-//          left = '-290px';
-//        }else{
-//         left = 0;
-//        }
-//      document.querySelector('#computer').style.background = 'url("gbb.GIF")' + left + 0;
-//     }, 100);
+Object.entries(딕셔너리) //객체를 배열형태로 바꿔준다
 
-// setInterval(() => {
-//     if (left === 0) {
-//         left = '-133px';
-//     } else if (left === '-133px') {
-//         left = '-290px';
-//     } else {
-//         left = 0;
-//     }
-//     console.log(left);
-//     document.getElementById('computer').style.backgroundPosition = left + '0';
+function 컴퓨터의선택(이미지좌표){
+    return Object.entries(딕셔너리).find(function(v){ //1번째가 이미지좌표인것을 찾아준다.
+                                                    //리턴이 true 인것을 찾는다
+        return v[1] === 이미지좌표;       
+    })[0];
+}
 
 
-// }, 100);
+setInterval(function () {
+    if (컴퓨터 === 딕셔너리.바위) {
+        컴퓨터 = 딕셔너리.가위;
+    } else if (컴퓨터 === 딕셔너리.가위) {
+        컴퓨터 = 딕셔너리.보;
+    } else {
+        컴퓨터 = 딕셔너리.바위;
+    }
+    document.getElementById('computer').style.backgroundPosition = 컴퓨터;
+}, 200);
 
-setInterval(() => {
-         if (left === 0) {
-             left = '-133px';
-         } else if (left === '-133px') {
-             left = '-288px';
-         } else {
-             left = 0;
-         }
-    
-    document.getElementById('computer').style.backgroundPosition = left;
-    console.log(left);
-     }, 200);
-    
+document.querySelectorAll('.btn').forEach(function (btn) {
+    btn.addEventListener('click', function () {
+       var 플레이어 = this.textContent;
+        console.log(플레이어, 딕셔너리2[컴퓨터]);
+    });
+});
