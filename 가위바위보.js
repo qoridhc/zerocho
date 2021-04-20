35fc54ad6750bc156ebd980ae630c5ed45fe279a
qoridhc <67674337+qoridhc@users.noreply.github.com>
2021-04-20 15:29:49 +0900

    Add files via upload

--- 가위바위보.js
+++ 가위바위보.js
@@ -1,34 +1,71 @@
-var 컴퓨터 = 0;
-var 딕셔너리 ={
-    바위 : '0',
-    가위 : '133px',
-    보 : '-288px'
+var 이미지좌표 = '0';
+var 가위바위보 = {
+    바위: '0',
+    가위: '-133px',
+    보: '-288px'
 };
 
-Object.entries(딕셔너리) //객체를 배열형태로 바꿔준다
-
-function 컴퓨터의선택(이미지좌표){
-    return Object.entries(딕셔너리).find(function(v){ //1번째가 이미지좌표인것을 찾아준다.
-                                                    //리턴이 true 인것을 찾는다
-        return v[1] === 이미지좌표;       
+function 컴퓨터의선택(이미지좌표) {
+    return Object.entries(가위바위보).find(function (v) { //1번째가 이미지좌표인것을 찾아준다.
+        //리턴이 true 인것을 찾는다
+        return v[1] === 이미지좌표;
     })[0];
 }
 
 
-setInterval(function () {
-    if (컴퓨터 === 딕셔너리.바위) {
-        컴퓨터 = 딕셔너리.가위;
-    } else if (컴퓨터 === 딕셔너리.가위) {
-        컴퓨터 = 딕셔너리.보;
+var 인터벌;
+function 인터벌메이커(){
+    setInterval(function () {
+    if (이미지좌표 === 가위바위보.바위) {
+        이미지좌표 = 가위바위보.가위;
+    } else if (이미지좌표 === 가위바위보.가위) {
+        이미지좌표 = 가위바위보.보;
     } else {
-        컴퓨터 = 딕셔너리.바위;
+        이미지좌표 = 가위바위보.바위;
     }
-    document.getElementById('computer').style.backgroundPosition = 컴퓨터;
-}, 200);
+    document.getElementById('computer').style.backgroundPosition = 이미지좌표;
+}, 300);
+}
+
+인터벌메이커();
 
 document.querySelectorAll('.btn').forEach(function (btn) {
     btn.addEventListener('click', function () {
-       var 플레이어 = this.textContent;
-        console.log(플레이어, 딕셔너리2[컴퓨터]);
+        clearInterval(인터벌);
+        
+        setTimeout(() => {
+            인터벌 = 인터벌메이커();
+        }, 1000);
+
+        var 나의선택 = this.textContent;
+        console.log("플레이어: " + 나의선택 + "\n" + "컴퓨터: " + 컴퓨터의선택(이미지좌표));
+    
+      //  tmp(가위바위보[나의선택],이미지좌표);
+     
+        if (나의선택 === '가위') {
+            if (컴퓨터의선택(이미지좌표) === '가위') {
+                console.log("비겼습니다.");
+            } else if (컴퓨터의선택(이미지좌표) === '바위') {
+                console.log("졌습니다ㅠㅠ.");
+            } else {
+                console.log("이겼습니다!!.");
+            }
+        } else if (나의선택 === '바위') {
+            if (컴퓨터의선택(이미지좌표) === '가위') {
+                console.log("이겼습니다!!.");
+            } else if (컴퓨터의선택(이미지좌표) === '바위') {
+                console.log("비겼습니다.");
+            } else {
+                console.log("졌습니다ㅠㅠ.");
+            }
+        } else if (나의선택 === '보') {
+            if (컴퓨터의선택(이미지좌표) === '가위') {
+                console.log("졌습니다ㅠㅠ.");
+            } else if (컴퓨터의선택(이미지좌표) === '바위') {
+                console.log("이겼습니다!!.");
+            } else {
+                console.log("비겼습니다.");
+            }
+        }
     });
 });
